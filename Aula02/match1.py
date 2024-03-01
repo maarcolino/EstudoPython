b55ef98b3305c90ad0c2ad9def309cc0c4c1f5e3
Enzo Marcolino <the3brozgames@gmail.com>
2024-03-01 14:19:14 -0300

    Aula-02 primeiro arquivo match adicionado

---
+++ Aula02/match1.py
@@ -0,0 +1,20 @@
+print("Este programa analisa os valores digitados")
+digito = input("Entre com um número e 0 a 6:")
+
+match digito:
+    case '0':
+        print("Domingo")
+    case '1':
+        print("Segunda-Feira")
+    case '2':
+        print("Terça-Feira")
+    case '3':
+        print("Quarta-Feira")
+    case '4':
+       print("Quinta-Feira")
+    case '5':
+        print("Sexta-Feira")
+    case '6':
+        print("Sábado")
+    case '_':
+        print("Valor incorreto. Tente outra vez")
